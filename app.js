98977f424b352aba5bae6d7a4ad30aa57ccfabe1
cpham19 <calvinthepham@gmail.com>
2018-03-20 00:06:58 -0700

    Leo's code with table and colors

--- app.js
+++ app.js
@@ -1,5 +1,90 @@
 const inquirer = require('inquirer'),
-      api = require('api')
+      api = require('api'),
+      colors = require('colors'),
+      Table = require('cli-table')
+
+    
+ 
+//need to install npm cli-table &  colors
+
+ 
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+
+//
+//*********table code */
+ 
+
+//findCompletedItems
+let findCompletedItemsTable = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+    head: [colors.verbose('Item#'), colors.verbose('Description')]
+
+})
+//findItemsAdvanced
+let findItemsAdvancedTable = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+    head: [colors.verbose('Item#'), colors.verbose('Description')]
+
+})
+//findItemsByCategory
+let findItemsByCategoryTable = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+    head: [colors.verbose('Item#'), colors.verbose('Description')]
+
+})
+//findItemsByKeywords
+let findItemsByKeywordsTable = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+    head: [colors.verbose('Item#'), colors.verbose('Description')]
+
+})
+//findItemsByProduct
+let findItemsByProductTable = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+    head: [colors.verbose('Item#'), colors.verbose('Description')]
+
+})
+//findItemsIneBayStores
+let findItemsIneBayStoresTable = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+    head: [colors.verbose('Item#'), colors.verbose('Description')]
+
+})
+
+//********************* */
+
+ 
+
 
 const findCompletedItems = (query) => {
     api.findCompletedItems(query).then(res => {
@@ -12,9 +97,16 @@ const findCompletedItems = (query) => {
         console.log("Completed Items for '" + query + "'")
         console.log('----------------')
         searchResults.forEach(obj => {
-            console.log(`Item #${counter}` + ": " + obj.title)
+            let objString = obj.title.toString()
+           
+            findCompletedItemsTable.push(
+            [colors.info(counter), colors.data(objString)]
+          
+            ); 
+          //  console.log(colors.info(`Item #${counter}`)+ ": " + colors.data(obj.title))
             counter++
         })
+        console.log(findCompletedItemsTable.toString());
     })
 }
 
@@ -29,9 +121,16 @@ const findItemsAdvanced = (query) => {
         console.log("Items Advanced for '" + query + "'")
         console.log('----------------')
         searchResults.forEach(obj => {
-            console.log(`Item #${counter}` + ": " + obj.title)
+            let objString = obj.title.toString()
+           
+            findItemsAdvancedTable.push(
+            [colors.info(counter), colors.data(objString)]
+          
+            ); 
+         //   console.log(colors.info(`Item #${counter}`)+ ": " + colors.data(obj.title))
             counter++
         })
+        console.log(findItemsAdvancedTable.toString());
     })
 }
 
@@ -46,9 +145,16 @@ const findItemsByCategory = (query) => {
         console.log("Items Based on Category ID #" + query)
         console.log('----------------')
         searchResults.forEach(obj => {
-            console.log(`Item #${counter}` + ": " + obj.title)
+            let objString = obj.title.toString()
+           
+            findItemsByCategoryTable.push(
+            [colors.info(counter), colors.data(objString)]
+          
+            ); 
+          //  console.log(colors.info(`Item #${counter}`)+ ": " + colors.data(obj.title))
             counter++
         })
+        console.log(findItemsByCategoryTable.toString());
     })
 }
 
@@ -62,10 +168,24 @@ const findItemsByKeywords = (query) => {
         let counter = 1
         console.log("Items using '" + query + "'")
         console.log('----------------')
+        
+      
+
         searchResults.forEach(obj => {
-            console.log(`Item #${counter}` + ": " + obj.title)
+            let objString = obj.title.toString()
+           
+    findItemsByKeywordsTable.push(
+    [colors.info(counter), colors.data(objString)]
+  
+    ); 
+         
+         //  console.log(colors.info(`Item #${counter}` + ": ") + colors.data( obj.title))
             counter++
         })
+        console.log(findItemsByKeywordsTable.toString());
+ 
+
+      
     })
 }
 
@@ -80,9 +200,16 @@ const findItemsByProduct = (query) => {
         console.log("Items using Product ID #'" + query + "'")
         console.log('----------------')
         searchResults.forEach(obj => {
-            console.log(`Item #${counter}` + ": " + obj.title)
+            let objString = obj.title.toString()
+           
+            findItemsByProductTable.push(
+            [colors.info(counter), colors.data(objString)]
+          
+            ); 
+          //  console.log(color.info(`Item #${counter}`) + ": " +colors.data(obj.title))
             counter++
         })
+        console.log(findItemsByProductTable.toString());
     })
 }
 
@@ -96,10 +223,18 @@ const findItemsIneBayStores = (query) => {
         let counter = 1
         console.log("Items in Store '" + query + "'")
         console.log('----------------')
+        
         searchResults.forEach(obj => {
-            console.log(`Item #${counter}` + ": " + obj.title)
+            let objString = obj.title.toString()
+           
+        findItemsIneBayStoresTable.push(
+            [colors.info(counter), colors.data(objString)]
+          
+            ); 
+           // console.log(colors.info(`Item #${counter}`)+ ": " + colors.data(obj.title))
             counter++
         })
+        console.log(findItemsIneBayStoresTable.toString());
     })
 }
 
